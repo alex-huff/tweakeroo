499c1ee9e1c03c85f76b7b8c5bd4e802fe3ea309
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2024-08-31 14:34:09 +0300

    Fix `tweakExplosionReducedParticles` not being updated to work in 1.20.4

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinExplosion.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinExplosion.java
@@ -1,31 +1,32 @@
 package fi.dy.masa.tweakeroo.mixin;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
-import net.minecraft.particle.DefaultParticleType;
+
+import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.world.explosion.Explosion;
+
 import fi.dy.masa.tweakeroo.config.FeatureToggle;
 
 @Mixin(Explosion.class)
 public abstract class MixinExplosion
 {
+    @Shadow @Final private ParticleEffect emitterParticle;
+
     @Redirect(method = "affectWorld",
-              slice = @Slice(
-                            from = @At("HEAD"),
-                            to = @At(value = "FIELD",
-                                     target = "Lnet/minecraft/world/explosion/Explosion;affectedBlocks:Lit/unimi/dsi/fastutil/objects/ObjectArrayList;")),
               at = @At(value = "FIELD",
-                       target = "Lnet/minecraft/particle/ParticleTypes;EXPLOSION_EMITTER:Lnet/minecraft/particle/DefaultParticleType;"))
-    private DefaultParticleType redirectSpawnParticles()
+                       target = "Lnet/minecraft/world/explosion/Explosion;emitterParticle:Lnet/minecraft/particle/ParticleEffect;"))
+    private ParticleEffect tweakeroo$reducedParticles$redirectEmitterParticle(Explosion instance)
     {
         if (FeatureToggle.TWEAK_EXPLOSION_REDUCED_PARTICLES.getBooleanValue())
         {
             return ParticleTypes.EXPLOSION;
         }
 
-        return ParticleTypes.EXPLOSION_EMITTER;
+        return this.emitterParticle;
     }
 }
